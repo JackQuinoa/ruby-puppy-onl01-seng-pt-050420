7b8d304fe43dc5f4a71afd3cb3f4f10de2033bf5
Jack Quinoa <jquinoa208@gmail.com>
2020-05-28 01:07:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self   
+    ##save 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.print_all 
+    @@all.each do |name|
+      puts "#{name}"
+    end
+  end
+  
+  ##def save 
+    ## @@name << self 
+  ##end
+end
